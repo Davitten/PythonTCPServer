ac366588917a5c5843f3eb16cd35efd3a2e1ba40
Martin Davidsen <ma.davidsen1@gmail.com>
2024-08-27 11:11:29 +0200

    Able to parse a single protobuf at a time. Needs support for multiple messages at a time by adding a prefixed length of message

--- tcpserver/tcpserver.py
+++ tcpserver/tcpserver.py
@@ -6,14 +6,24 @@ class TCPHandler(socketserver.StreamRequestHandler):
     def handle(self):
         # self.rfile is a file-like object created by the handler;
         # we can now use e.g. readline() instead of raw recv() calls
-        data = self.rfile.readline().strip()
+        data = self.rfile.read()
         print(f"Received data from host: {self.client_address[0]}:")
         print(data)
-        # a = addressbook_pb2.Person()
-        # print(a.ParseFromString(data))
+        a = addressbook_pb2.Person()
+        a.ParseFromString(data)
+        print(a)
         # Likewise, self.wfile is a file-like object used to write back
         # to the client
         self.wfile.write(data.upper())
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        data = self.request.recv(1024).strip()
+        print(self.client_address)
+        print("Received from {}:".format(self.client_address[0]))
+        print(data)
 
 
 class TCPServer:
